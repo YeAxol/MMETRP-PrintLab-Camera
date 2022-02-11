f8dd4fd1cd1c62b3b9846f783ac7bc411af28452
YeAxol <16109223+YeAxol@users.noreply.github.com>
2022-02-11 11:50:25 -0500

    FINAL WORKING CODE

--- cameras.py
+++ cameras.py
@@ -1,16 +1,5 @@
-
-from flask import Flask, render_template, request, make_response
-from os import listdir
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
-#Static Routes
-@app.route('/ben')
-def ben_cam():
-    return render_template('ben.html')
-
-@app.route('/sam')
-def sam_cam():
-    return render_template('sam.html')
 
 @app.route('/', methods=['GET', 'POST'] )
 def remote():
@@ -31,19 +20,14 @@ def remote():
                 file.close()
                 return render_template('default.html')
 
-
-
 @app.route('/control', methods=['POST','GET'])
 def control():
-    input_nopol = request.form['text_box']
     if request.method == 'POST':
-        with open('state1.txt', 'w+') as f:
+        input_nopol = request.form['text_box']
+        with open('state.txt', 'w+') as f:
                 f.write(str(input_nopol))
         return render_template('control.html', nopol=input_nopol)
-    else:
-        return render_template('control.html')
-
-    
+    return render_template('control.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
